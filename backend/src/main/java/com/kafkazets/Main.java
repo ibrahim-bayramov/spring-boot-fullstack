e037fec025dc4c8f18e1ac148b3f9044d9e97977
ibrahim-bayramov <ibrahim-bayramov@hotmail.com>
2023-12-26 21:24:59 +0100

    added new line and removed unused import

--- backend/src/main/java/com/kafkazets/Main.java
+++ backend/src/main/java/com/kafkazets/Main.java
@@ -8,8 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
@@ -17,6 +15,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
     }
 
     @Bean
